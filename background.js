eacc13808527105f6989dcc818ecb25e0d63152d
Ya-An Hsiung <ya-anhsiung@berkeley.edu>
2017-10-02 15:34:48 -0700

    added background

---
+++ background.js
@@ -0,0 +1,28 @@
+// chrome.runtime.onInstalled.addListener(function() {
+//   // Replace all rules ...
+//   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+//     // With a new rule ...
+//     chrome.declarativeContent.onPageChanged.addRules([
+//       {
+//         // That fires when a page's URL contains a 'g' ...
+//         conditions: [
+//           new chrome.declarativeContent.PageStateMatcher({
+//             pageUrl: { urlContains: 'https://www.youtube.com' },
+//           })
+//         ],
+//         // And shows the extension's page action.
+//         actions: [ new chrome.declarativeContent.ShowPageAction() ]
+//       }
+//     ]);
+//   });
+// });
+function checkForValidUrl(tabId, changeInfo, tab) {
+// If the tabs url starts with "http://specificsite.com"...
+	if (tab.url.indexOf('https://www.youtube.com') == 0) {
+// ... show the page action.
+		chrome.pageAction.show(tabId);
+	}
+};
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
