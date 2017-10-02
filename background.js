7387abba738661fca680642aed143204ba4bb512
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-10-02 16:37:25 -0700

    added popup & styling when icon clicked

--- background.js
+++ background.js
@@ -1,28 +1,18 @@
 chrome.runtime.onInstalled.addListener(function() {
-  // Replace all rules ...
+  // replace all rules
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    // With a new rule ...
+    // with a new rule
     chrome.declarativeContent.onPageChanged.addRules([
       {
-        // That fires when a page's URL contains a 'g' ...
+        // that fires when a page's URL contains "youtube.com"
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'youtube' },
+            pageUrl: { urlContains: 'youtube.com' },
           })
         ],
-        // And shows the extension's page action.
+        // show the extension's page action
         actions: [ new chrome.declarativeContent.ShowPageAction() ]
       }
     ]);
   });
 });
-// function checkForValidUrl(tabId, changeInfo, tab) {
-// // If the tabs url starts with "http://specificsite.com"...
-// 	if (tab.url.indexOf('https://www.youtube.com') == 0) {
-// // ... show the page action.
-// 		chrome.pageAction.show(tabId);
-// 	}
-// };
-
-// // Listen for any changes to the URL of any tab.
-// chrome.tabs.onUpdated.addListener(checkForValidUrl);
