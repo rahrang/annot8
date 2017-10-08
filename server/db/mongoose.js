a827919eaae62b8b95aa0284e3d438de77809b3b
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-10-08 12:51:20 -0700

    connected heroku to mlab mongodb database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Anot8');
+
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/Anot8');
 
 module.exports = {mongoose}
