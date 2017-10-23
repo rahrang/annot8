1949c37806c8dd5ad83dee7b49ffcbd0a382ba76
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-10-22 19:54:10 -0700

    modified GoogleStrategy settings

--- server/services/passport.js
+++ server/services/passport.js
@@ -19,9 +19,10 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  }, (accessToken, refreshToken, profile, done) => {
-    
+    callbackURL: '/auth/google/callback',
+    proxy: true
+  },
+  (accessToken, refreshToken, profile, done) => {
     User.findOne({ googleId: profile.id })
     .then((existingUser) => {
         if (existingUser) {
