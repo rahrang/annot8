91a83ea11c11881f6ba620725c35b91dde162dbe
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-10-22 19:48:44 -0700

    finish oauth flow

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID: '811622678647-qpbisooq5re68roir0a94usrfhplq1oc.apps.googleusercontent.com',
-    googleClientSecret: 'eSZY7B9uCGjd6E_DwW1sbzVy',
-    mongoURI: 'mongodb://rahul:annot8-dev-password@ds229835.mlab.com:29835/annot8-dev',
-    cookieKey: 'jaslkjfc32423kjksdajflksadjhfka3alksdfjkas7'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod.js');
+} else {
+    module.exports = require('./dev.js');
 }
