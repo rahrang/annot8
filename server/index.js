f3fc3376a8c0881072b77133ab79b5c41b9df0f2
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-10-24 17:16:55 -0700

    another fix attempt

--- server/index.js
+++ server/index.js
@@ -38,13 +38,13 @@ require('./routes/authRoutes.js')(app);
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-  app.use(express.static('./client/build'));
+  app.use(express.static('client/build'));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   });
 }
 
