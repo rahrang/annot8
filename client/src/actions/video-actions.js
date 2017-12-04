05d64a09b418907dadfe08a7053965320419bedf
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-12-04 00:38:11 -0800

    still working to connect to YouTube API -- you cannot get information on private videos and I don't think OAuth helps

--- client/src/actions/video-actions.js
+++ client/src/actions/video-actions.js
@@ -8,7 +8,6 @@ export const VideoActions = {
   fetchVideoStats: videoId => async dispatch => {
     const params = { videoId };
     const res = await axios.post('/api/video/stats', params);
-    console.log(res); // TODO
     dispatch({
       type: VideoConstants.FETCH_VIDEO_COMMENTS,
       title: res.data.title,
--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -2,26 +2,37 @@ const keys = require('../config/keys.js');
 const _ = require('lodash');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube(keys.googleAPIKey);
+const axios = require('axios');
 
 module.exports = app => {
   // GET the title and duration for a video with the given id
+  // app.post('/api/video/stats', async (req, res) => {
+  //   const { videoId } = req.body;
+  //   console.log(videoId);
+  //   youtube
+  //     .getVideo(`https://www.youtube.com/watch?v=${videoId}`)
+  //     .then(results => console.log(results))
+  //     .catch(results => console.log(results));
+  //   // const video = await youtube.getVideo(
+  //   //   `https://www.youtube.com/watch?v=${videoId}`
+  //   // );
+  //   // console.log('HERE');
+  //   // if (video) {
+  //   //   const title = video[0].title;
+  //   //   const duration = video[0].duration;
+  //   //   return res.status(200).send({ title, duration });
+  //   // }
+  //   return res.status(422).send({ title: '', duration: '' });
+  // });
+
   app.post('/api/video/stats', async (req, res) => {
     const { videoId } = req.body;
     console.log(videoId);
-    youtube
-      .getVideo(`https://www.youtube.com/watch?v=${videoId}`)
-      .then(results => console.log(results))
-      .catch(results => console.log(results));
-    // const video = await youtube.getVideo(
-    //   `https://www.youtube.com/watch?v=${videoId}`
-    // );
-    // console.log('HERE');
-    // if (video) {
-    //   const title = video[0].title;
-    //   const duration = video[0].duration;
-    //   return res.status(200).send({ title, duration });
-    // }
-    return res.status(422).send({ title: '', duration: '' });
+    const query = await axios.get(
+      `https://www.googleapis.com/youtube/v3/videos?key=${keys.googleAPIKey}&part=contentDetails,snippet&id=Hp7C5TaHcFw`
+    );
+    console.log(query.data);
+    res.status(404).send({ title: 'HERE', duration: '10' });
   });
 };
 
